c2e495753d697028d5eea35e3009130d8042f383
Fokoda799 <fey2003@gmail.com>
2024-08-14 16:39:18 +0200

    fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- Author: Abdellah
 -- Description: SQL script to create the session table
-CREATE TABLE IF NOT EXISTS
-users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    contry ENUM('US', 'CO', 'TN') DEFAULT 'US'
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
