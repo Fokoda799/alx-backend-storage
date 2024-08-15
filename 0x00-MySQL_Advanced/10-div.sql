e097692f803d720348e173cf9e8f800ff531d8f9
Fokoda799 <fey2003@gmail.com>
2024-08-15 19:46:52 +0200

    ok

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,21 @@
+-- Content: MySQL function to safely divide two numbers
+-- MySQL Version: 5.7.31
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS SafeDiv$$
+
+CREATE FUNCTION SafeDiv(
+    a INT,
+    b INT
+)
+RETURNS INT
+BEGIN
+    -- Check if the divisor is zero
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
