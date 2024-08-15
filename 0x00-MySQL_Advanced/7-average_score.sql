21b8381cfcfb4bdd8b99b851b30b28104142cbfe
Fokoda799 <fey2003@gmail.com>
2024-08-15 17:44:31 +0200

    ok

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,28 +1,42 @@
 -- Average score
 -- Write a stored procedure ComputeAverageScoreForUser that computes the average score of a user.
+-- DELIMITER $$
+
 DELIMITER $$
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser$$
+
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+    IN v_user_id INT
 )
 BEGIN
-    DECLARE average_score FLOAT;
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
+    DECLARE total_score FLOAT DEFAULT 0.0;
+    DECLARE total_projects INT DEFAULT 0;
+    DECLARE v_average_score FLOAT DEFAULT 0.0;
 
-    SELECT average_score INTO average_score
-    FROM users 
-    WHERE id = user_id;
+    SELECT average_score
+    INTO v_average_score
+    FROM users
+    WHERE id = v_user_id;
 
-    SELECT SUM(score) INTO total_score,
-    COUNT(project_id) INTO total_projects
+    -- Calculate total score and total projects for the user
+    SELECT SUM(score), COUNT(*)
+    INTO total_score, total_projects
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = v_user_id;
 
+    -- Calculate average score if there are projects
     IF total_projects = 0 THEN
-        SET average_score = 0;
+        SET v_average_score = 0.0;
     ELSE
-        SET average_score = total_score / total_projects;
+        SET v_average_score = total_score / total_projects;
     END IF;
+
+    -- Update the average score for the user
+    UPDATE users
+    SET average_score = v_average_score
+    WHERE id = v_user_id;
 END$$
+
 DELIMITER ;
+
