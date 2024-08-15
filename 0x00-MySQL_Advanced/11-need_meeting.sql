5a687b716b1c4d4ceb377ad5749fc8989edecea4
Fokoda799 <fey2003@gmail.com>
2024-08-15 20:23:37 +0200

    ok

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,10 +2,11 @@
 -- Describe: Create a view named need_meeting that will return the name of students that need a meeting.
 -- A student needs a meeting if his score is below 80 or if his last_meeting is older than 30 months.
 -- The view should return the name of the students that need a meeting.
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE score < 80
-   OR last_meeting IS NULL
+WHERE (score < 80
+   AND last_meeting IS NULL)
    OR last_meeting < DATE_SUB(NOW(), INTERVAL 30 MONTH);
 
